307655c73c9ae6ffc0465f921ededa5425169c3d
Mariusz <kontakt@molszowski.pl>
2016-01-04 20:34:57 +0100

    Obsługa przecinka

--- app/code/local/Monogo/Currency/controllers/IndexController.php
+++ app/code/local/Monogo/Currency/controllers/IndexController.php
@@ -10,7 +10,7 @@ class Monogo_Currency_IndexController extends Mage_Core_Controller_Front_Action
         ///$this->getChild('content')->setHtml('asd');
         
         if( $Form->isFilled() ) {
-            $Currency = (float) $_POST['Currency'];
+            $Currency = (float) str_replace( ',', '.', $_POST['Currency'] );
             list( $KursK, $KursS ) = $Form->getCurrency();
             $this->getLayout()->getBlock('content')->setText( $Form->getResult( round($Currency * $KursS, 2), round($Currency * $KursK, 2) ) );
         }
